1c15007a264c520fcf73bfe7a9782873124b5fdc
Quater <102786860+quanganh296@users.noreply.github.com>
2024-12-13 11:24:00 +0700

    Add files via upload

--- CacSoNguyenToDauTien.cpp
+++ CacSoNguyenToDauTien.cpp
@@ -1,17 +1,22 @@
 #include <stdio.h>
+#include<math.h>
 
 int main() {
-   int n;
+   int n,count;
    printf("Hay nhap mot so nguyen duong: ");
    scanf("%d",&n);
-   	printf(" Cac so nguyen to dau tien den %d la:",n);
-   for(int i=2;i<=n;i++)
-   
-   if(i>=2&&i%2!=0||i==2)
+   if(n<0){
+   	printf("hay nhap lai mot so nguyen duong");
+   	return 0;
+   }
+   printf(" cac so nguyen to dau tien la:");
+   for(int i=2;i<=sqrt(count)&&i<=n;i++)
+   if(count%i!=0||i==2)
    {
-   	printf(" %d",i);
+ 
    
-	} 
+		printf(" %d",i);
+	}
 
     return 0;
 }
